13604638093124813d91f38816824ed87caab3a2
Daniel Miller <dmiller@angrygiant.com>
2012-01-17 16:11:44 -0500

    addition of liquibase use with application
    migration to use postgresql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,31 +13,23 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+            url = "jdbc:postgresql://localhost:5432/funtastik-dev"
+            driverClassName = "org.postgresql.Driver"
+            username = "funtastik"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+            url = "jdbc:postgresql://localhost:5432/funtastik-test"
+            driverClassName = "org.postgresql.Driver"
+            username = "funtastik"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:postgresql://localhost:5432/funtastik-prod"
+            driverClassName = "org.postgresql.Driver"
+            username = "funtastik"
         }
     }
 }
